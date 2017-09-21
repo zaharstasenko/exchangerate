1de39f8602f4385c0576d1b6e2dfaeec3e85e59a
zakhariy.stasenko <zakhariy.stasenko@globallogic.com>
2017-09-21 17:00:59 +0300

    Feature 1 (fixes 2.0)

---
+++ app/src/main/java/com/example/zakhariystasenko/exchangerate/ViewHolder.java
@@ -0,0 +1,21 @@
+package com.example.zakhariystasenko.exchangerate;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+class ViewHolder extends RecyclerView.ViewHolder {
+    TextView mCurrencyName;
+    ImageView mCurrencyImage;
+    TextView mCurrencyId;
+    TextView mCurrencyRate;
+
+    ViewHolder(View v) {
+        super(v);
+        mCurrencyName = v.findViewById(R.id.currency_name);
+        mCurrencyImage = v.findViewById(R.id.currency_image);
+        mCurrencyId = v.findViewById(R.id.currency_id);
+        mCurrencyRate = v.findViewById(R.id.currency_rate);
+    }
+}
